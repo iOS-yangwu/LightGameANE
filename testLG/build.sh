3fb75608e0f41e9248b885803bb575e43210366b
yang.wu <yang.wu@joypac.cn>
2020-10-16 11:06:40 +0800

    add demo

---
+++ testLG/build.sh
@@ -0,0 +1,2 @@
+adt -package -target ipa-test -hideAneLibSymbols no -storetype pkcs12 -keystore Certificate/test1_developer.p12 -storepass 123456 -provisioning-profile Certificate/dev.mobileprovision dev_ios.ipa dev_ios-app.xml test1.swf Default~iphone.png Default@2x~iphone.png Default-568h@2x~iphone.png Default-812h@3x~iphone.png Default-Portrait~ipad.png -extdir exdir
+
